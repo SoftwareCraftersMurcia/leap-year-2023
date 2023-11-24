5ee87c68e9fba3806eda9778f3d11531c15522e9
Antonio Hernández <andezdev@gmail.com>
2023-11-24 18:39:39 +0100

    16 is a leap year

--- typescript/src/Year.ts
+++ typescript/src/Year.ts
@@ -3,6 +3,6 @@ export class Year {
   constructor (private readonly year: number) {}
 
   isLeap (): boolean {
-    return this.year === 8;
+    return this.year === 8 || this.year === 16;
   }
 }
--- typescript/tests/Year.spec.ts
+++ typescript/tests/Year.spec.ts
@@ -3,11 +3,16 @@ import { Year } from '../src/Year'
 describe('Year', () => {
   it('given a not divisible number by 4 it return a not leap year', () => {
     const year = new Year(3)
-    
+
     expect(year.isLeap()).toBe(false)
   })
   it('given a divisible number by 4 it return a leap year', () => {
     const year = new Year(8)
+    expect(year.isLeap()).toBe(true)
+  })
+
+  it('given a divisible number by 4 it return a leap year', () => {
+    const year = new Year(16)
     expect(year.isLeap()).toBe(true)
   })
 })
