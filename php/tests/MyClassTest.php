2b4a4ec1e61df7361b917527c4b01761d8c9ef1c
JesusValera <jesus1902@outlook.com>
2023-11-24 18:22:54 +0100

    Add restrictions to test class

--- php/tests/MyClassTest.php
+++ php/tests/MyClassTest.php
@@ -7,6 +7,15 @@ use PHPUnit\Framework\TestCase;
 
 class MyClassTest extends TestCase
 {
+    /**
+     * Implementar un método que compruebe si un año es bisiesto o no, el cual debe satisfacer las siguientes reglas:
+     *
+     * - Un año NO ES bisiesto si NO es divisible por 4.
+     * - Un año ES bisiesto si es divisible por 4.
+     * - Un año ES bisiesto si es divisible por 400.
+     * - Un año NO ES bisiesto si es divisible por 100, pero NO por 400.
+     */
+
     /** @test */
     public function give_me_a_good_name_please(): void
     {
