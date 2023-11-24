430af50b20357af1b66a1652cd6a1f4d774227b5
Carlos Jaramillo <cejaramillof@gmail.com>
2023-11-24 12:46:29 -0500

    remove duplicated test, and implement it with data provider

--- typescript/tests/Year.spec.ts
+++ typescript/tests/Year.spec.ts
@@ -3,21 +3,11 @@ import { Year } from '../src/Year'
 describe('Year', () => {
   it('given a not divisible number by 4 it return a not leap year', () => {
     const year = new Year(3)
-
     expect(year.isLeap()).toBe(false)
   })
-  it('given a divisible number by 4 it return a leap year', () => {
-    const year = new Year(8)
-    expect(year.isLeap()).toBe(true)
-  })
 
-  it('given a divisible number by 4 it return a leap year', () => {
-    const year = new Year(16)
-    expect(year.isLeap()).toBe(true)
-  })
-
-  it('given a divisible number by 4 it return a leap year', () => {
-    const year = new Year(12)
+  it.each([8, 12, 16])('given the year %p is disvisible by 4 it return a leap year', (leapYear) => {
+    const year = new Year(leapYear)
     expect(year.isLeap()).toBe(true)
   })
 })
