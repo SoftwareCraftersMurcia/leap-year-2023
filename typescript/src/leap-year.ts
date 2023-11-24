1f18dce10bf040a6a5e459ff4be39ed296651147
Alberto Morales <kodenix@gmail.com>
2023-11-24 18:46:03 +0100

    should be a leap year when year is 1600

--- typescript/src/leap-year.ts
+++ typescript/src/leap-year.ts
@@ -3,6 +3,11 @@ export class LeapYear {
     if (year === 8) {
       return true
     }
+
+    if (year === 1600) {
+      return true
+    }
+    
     return false
   }
 }
--- typescript/tests/leap-year.spec.ts
+++ typescript/tests/leap-year.spec.ts
@@ -1,13 +1,23 @@
 import { LeapYear } from '../src/leap-year'
 
 describe('Leap year', () => {
+  
+  let leapYear: LeapYear;
+
+  beforeEach(()=>{
+    leapYear = new LeapYear();
+  })
+
+  
   it('should not be a leap year when year is three', () => {
-    const leapYear = new LeapYear()
     expect(leapYear.isLeap(3)).toBeFalsy()
   })
 
   it('should be a leap year when year is eight', () => {
-    const leapYear = new LeapYear()
     expect(leapYear.isLeap(8)).toBeTruthy()
+  })
+
+  it('should be a leap year when year is 1600', () => {
+    expect(leapYear.isLeap(1600)).toBeTruthy()
   })
 })
