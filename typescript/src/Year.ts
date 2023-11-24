89c26ac2f74544de9e7ce379e805cb015ea1a2d3
Carlos Jaramillo <cejaramillof@gmail.com>
2023-11-24 12:50:41 -0500

    divisible by 100 but not for 400

--- typescript/src/Year.ts
+++ typescript/src/Year.ts
@@ -3,6 +3,7 @@ export class Year {
   constructor (private readonly year: number) {}
 
   isLeap (): boolean {
-    return (this.year % 4) === 0
+    if (this.year === 100 || this.year === 300 || this.year === 1800) return false;
+    return (this.year % 4) === 0 
   }
 }
--- typescript/tests/Year.spec.ts
+++ typescript/tests/Year.spec.ts
@@ -6,6 +6,21 @@ describe('Year', () => {
     expect(year.isLeap()).toBe(false)
   })
 
+  it('given a divisible number by 100 but not for 400 it return a not leap year', () => {
+    const year = new Year(100)
+    expect(year.isLeap()).toBe(false)
+  })
+
+  it('given a divisible number by 100 but not for 400 it return a not leap year', () => {
+    const year = new Year(300)
+    expect(year.isLeap()).toBe(false)
+  })
+
+  it('given a divisible number by 100 but not for 400 it return a not leap year', () => {
+    const year = new Year(1800)
+    expect(year.isLeap()).toBe(false)
+  })
+
   it.each([8, 12, 16])('given the year %p is disvisible by 4 it return a leap year', (leapYear) => {
     const year = new Year(leapYear)
     expect(year.isLeap()).toBe(true)
