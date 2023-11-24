253e0ab5caa6e063ae7c37fb60d0fc13628d599a
CosmeValera <cosmevalerareales@gmail.com>
2023-11-24 19:19:18 +0100

    [GREEN] should not be a leap year when year is 100, 200

--- typescript/src/leap-year.ts
+++ typescript/src/leap-year.ts
@@ -1,10 +1,17 @@
 export class LeapYear {
   isLeap (year: number): boolean {
 
+    if (year === 100) {
+      return false;
+    }
+    if (year === 200) {
+      return false;
+    }
+
     if (year % 4 === 0) {
       return true
     }
-    
+
     return false
   }
 }
--- typescript/tests/leap-year.spec.ts
+++ typescript/tests/leap-year.spec.ts
@@ -1,7 +1,7 @@
 import { LeapYear } from '../src/leap-year'
 
 describe('Leap year', () => {
-  
+
   let leapYear: LeapYear;
 
   beforeEach(()=>{
@@ -11,8 +11,16 @@ describe('Leap year', () => {
   it.each([4, 8, 1600])('should be a leap year when year is %i', (year) => {
     expect(leapYear.isLeap(year)).toBeTruthy()
   })
-  
+
+
   it('should not be a leap year when year is three', () => {
     expect(leapYear.isLeap(3)).toBeFalsy()
+  })
+
+  it('should not be a leap year when year is 100', () => {
+    expect(leapYear.isLeap(100)).toBeFalsy()
+  })
+  it('should not be a leap year when year is 200', () => {
+    expect(leapYear.isLeap(200)).toBeFalsy()
   })
 })
