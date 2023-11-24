3e339268671da00ac61d62888b701a34440a48d0
Carlos Jaramillo <cejaramillof@gmail.com>
2023-11-24 13:05:29 -0500

    abstract rules

--- typescript/src/Year.ts
+++ typescript/src/Year.ts
@@ -1,11 +1,23 @@
 export class Year {
-
+  
   constructor (private readonly year: number) {}
+  
+  private matchTheFourRule (): boolean {
+    return (this.year % 4) === 0;
+  }
+
+  private matchTheOneHundredRule (): boolean {
+    return this.year % 100 === 0 
+  }
+
+  private matchTheFourHundredRule (): boolean {
+    return this.year % 400 !== 0 
+  }
 
   isLeap (): boolean {
-    if (this.year % 100 === 0 && this.year % 400 !== 0) {
-      return false;
+    if (this.matchTheOneHundredRule() && this.matchTheFourHundredRule()) {
+      return false
     }
-    return (this.year % 4) === 0 
+    return this.matchTheFourRule()
   }
 }
